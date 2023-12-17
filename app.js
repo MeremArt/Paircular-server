76ff52cde8f41c6f0b1c165ceea2cba7a3169387
Ugo Chinememerem <ugofranklin22@gmail.com>
2023-12-18 00:10:32 +0100

    trust issues

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
