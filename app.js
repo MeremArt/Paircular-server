2a4956a46f0d12d8449cfba61ab76a41e1f2b1cd
Ugo Chinememerem <ugofranklin22@gmail.com>
2023-12-23 11:38:32 +0100

    db-added

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
-const bodyParser = require("body-parser");
+const app = express(`./db/connect.js`);
+
 const mongoose = require("mongoose");
+const connectDB = require(`./`);
 const cors = require("cors");
 const tasksRouter = require(`./routes/tasks`);
 const notFoundMiddleware = require(`./middleware/not-found.js`);
 const errorMiddleware = require(`./middleware/error-handler.js`);
-require("dotenv").config();
-
-const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Connect to MongoDB Atlas using environment variable
-const atlasConnectionUri = process.env.MONGO_URL;
-
-mongoose.connect(atlasConnectionUri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
 app.use(`/api/v1/paircular-holmes`, tasksRouter);
 
@@ -39,6 +30,17 @@ process.on("SIGINT", () => {
     process.exit(0);
   });
 });
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+
+//Connect to MongoDB Atlas using environment variable
+const atlasConnectionUri = process.env.MONGO_URL;
+
+const start = async () => {
+  try {
+    await connectDB(atlasConnectionUri);
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
