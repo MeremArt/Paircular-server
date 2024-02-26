51704298cea900d6093fc7359ea3919082c5fffa
Ugo Chinememerem <ugofranklin22@gmail.com>
2024-02-26 01:20:47 +0100

    add: improved level of security

--- app.js
+++ app.js
@@ -1,41 +1,46 @@
 require("dotenv").config();
 require("express-async-errors");
 
+const helmet = require(`helmet`);
+const cors = require(`cors`);
+const xss = require(`xss-clean`);
+const rateLimit = require(`express-rate-limit`);
 const express = require("express");
-const app = express(`./db/connect.js`);
+const app = express();
 
 const mongoose = require("mongoose");
 const connectDB = require(`./db/connect`);
 const authenticateUser = require(`./middleware/auth`);
-const cors = require("cors");
 const cloudinary = require("cloudinary").v2;
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
-});
 const fileUpload = require("express-fileupload");
 const tasksRouter = require(`./routes/tasks`);
-
 const notFoundMiddleware = require(`./middleware/not-found.js`);
 const errorMiddleware = require(`./middleware/error-handler.js`);
 
 // Middleware
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+
 app.use(cors());
 app.use(express.json());
-
+app.use(helmet());
+app.use(xss());
 app.use(express.static("./public"));
 app.use(fileUpload({ useTempFiles: true }));
+
 app.use(`/api/v1/paircular-holmes`, tasksRouter);
 
-//ErrorHandling
+// Error Handling Middleware
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
 
 const port = process.env.PORT || 7000;
 
 // Graceful shutdown
-
 process.on("SIGINT", () => {
   mongoose.connection.close(() => {
     console.log("MongoDB disconnected through app termination");
@@ -43,7 +48,7 @@ process.on("SIGINT", () => {
   });
 });
 
-//Connect to MongoDB Atlas using environment variable
+// Connect to MongoDB Atlas using environment variable
 const atlasConnectionUri = process.env.MONGO_URL;
 
 const start = async () => {
