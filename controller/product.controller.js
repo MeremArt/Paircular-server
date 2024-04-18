aabe0a2e7e798af2cbec66a57b10f163d5a841b3
Verike <verike7@gmail.com>
2024-04-18 18:16:16 -0400

    fix: search index filter adjustment

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -4,12 +4,25 @@ const { StatusCodes } = require("http-status-codes");
 const searchIndexProducts = async (req, res) => {
     try {
         const { location, availabilityDate, occupants, page = 1, limit = 5 } = req.query;
-        const query = {
-            location: location || { $exists: true },
-            availabilityDate: { $gte: new Date(availabilityDate || new Date()) },
-            occupants: { $gte: occupants || 1 },
-        };
 
+        const query = {};
+
+        if (location) query.location = location // Default to empty string if location is not provided
+
+        if (occupants) {
+            query.occupants = parseInt(occupants)
+        } else {
+            query.occupants = { $gte: 1 }
+        }; // Default to at least 1 occupant if not provided
+
+        if (availabilityDate) {
+            query.availabilityDate = { $gte: new Date(availabilityDate) }
+        } else {
+            query.availabilityDate = { $gte: new Date() } // Default to current date if availabilityDate is not provided
+        }
+
+        // console.log({ query })
+        // console.log({ date: new Date().toUTCString() })
         const options = {
             limit: parseInt(limit),
             skip: (page - 1) * limit,
@@ -26,6 +39,7 @@ const searchIndexProducts = async (req, res) => {
             pagination: {
                 currentPage: parseInt(page),
                 totalPages,
+                totalResults: totalCount,
                 hasNextPage: page < totalPages,
                 hasPrevPage: page > 1,
             },
@@ -36,7 +50,26 @@ const searchIndexProducts = async (req, res) => {
     }
 }
 
+const getProductById = async (req, res) => {
+    try {
+        const { productId } = req.params;
+
+        const existingProduct = await productService.getProductById(productId);
+
+        if (!existingProduct) {
+            return res
+                .status(StatusCodes.NOT_FOUND)
+                .json({ error: "Product not found" });
+        }
+        res.status(StatusCodes.OK).json({ success: true, data: existingProduct });
+    } catch (error) {
+        console.error(error);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Internal Server Error");
+    }
+}
+
 
 module.exports = {
     searchIndexProducts,
+    getProductById,
 };
